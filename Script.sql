06719c5a42e0a9f09989e8a1be436711805f9dd3
Mark Ayles <marka2049@gmail.com>
2022-02-14 15:38:35 -0500

    add testing sql file

--- Script.sql
+++ Script.sql
@@ -8,3 +8,12 @@ select * from employees;
  * show all the employees in office number
  * */
 select * from employees where officeCode  = 4;
+
+SELECT count(*) FROM employees;
+
+select distinct orderNumber from orderdetails;
+
+
+select * from offices;
+insert into offices values('8', 'Plano', '5554443333', '123 Street', null, 'TX', 'USA', '12345', 'NA');
+delete from offices where officeCode = '8';
